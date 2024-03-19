4a2c6b40a0ac953b2e579d40e1a8ccc6308cc5d0
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-03-18 23:04:49 -0300

    Delete código-projeto-ip-python directory

--- código-projeto-ip-python/arquivos_antigos/herois.py
+++
@@ -1,8 +0,0 @@
-class Personagem:
-    def __init__(self, stats, ataques_obtidos, ataques_disponiveis):
-        self.stats = stats
-        self.ataques_obtidos = ataques_obtidos
-        self.ataques_disponiveis = ataques_disponiveis
-
-    
-    #falta criar funções de se movimentar, atacar, etc.
