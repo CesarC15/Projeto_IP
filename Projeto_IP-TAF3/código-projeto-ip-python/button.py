5ca5420d5202705010ccb28a68abd4c80a1a9fa1
Tiago-A-Ferreira <161096748+Tiago-A-Ferreira@users.noreply.github.com>
2024-03-20 23:46:17 -0300

    Add files via upload

---
+++ Projeto_IP-TAF3/código-projeto-ip-python/button.py
@@ -0,0 +1,32 @@
+import pygame
+
+"REVER O CÓDIGO, A FUNÇÃO DE GAME_OVER ESTRANHAMENTE NÃO FUNCIONA E AQUI É A ÚNICA RELAÇÃO COM A FUNÇÃO;" 
+"REVER O QUE PODE ESTAR DE ERRADO POR AQUI (MÉDIA PRIORIDADE)"
+
+class Button:
+    def __init__(self, x, y, width, height, fg, bg, content, fontsize):
+        self.font = pygame.font.Font('arial.ttf', fontsize)
+        self.content = content
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.fg = fg
+        self.bg = bg
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.bg)        
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        self.text = self.font.render(self.content, True, self.fg)
+        self.text_rect = self.text.get_rect(center=(self.width/2, self.height/2))
+
+        self.image.blit(self.text, self.text_rect)
+
+    def is_pressed(self, pos, pressed):
+        if self.rect.collidepoint(pos):
+            return pressed[0]
+        return False
