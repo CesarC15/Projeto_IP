3213b8c2639c797616155a74d155aabb78b0636e
CesarC15 <159570682+CesarC15@users.noreply.github.com>
2024-03-21 11:17:16 -0300

    Update wall.py

--- Escape Out/wall.py
+++ Escape Out/wall.py
@@ -3,21 +3,30 @@ from config import *
 from spritesheet import *
 
 class Wall(pygame.sprite.Sprite):
-    def __init__(self, game, x, y):
+    def __init__(self, game, x, y, type):
         self.game = game
         self._layer = BLOCK_LAYER
         self.groups = self.game.all_sprites, self.game.blocks
         pygame.sprite.Sprite.__init__(self, self.groups)
+        self.type = type
 
         self.x = x * TILESIZE
         self.y = y * TILESIZE
         self.width = TILESIZE
         self.height = TILESIZE
 
-        self.image = self.game.terrain_spritesheet.get_sprite(WALL_X, WALL_Y, self.width, self.height)  
+        if self.type == 'A':
+            self.image = pygame.image.load('./img/select.png')
+        else:
+            self.image = pygame.image.load('./img/parede2.png')
+
         self.image.set_colorkey(BLACK)
+
+        self.image = pygame.transform.scale(self.image, (32, 32))
         
         self.rect = self.image.get_rect()
+
+
 
         self.rect.x = self.x
         self.rect.y = self.y
