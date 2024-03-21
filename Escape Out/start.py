3fea60ce18e5e604dda097e76b28a1b4048ab9b1
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-03-21 08:27:13 -0300

    Update start.py

--- Escape Out/start.py
+++ Escape Out/start.py
@@ -8,7 +8,9 @@ g.new()
 while g.running:
     g.main()
     if g.running:
-        if g.azul != 'OK' and g.verde != 'OK' and g.vermelho != 'OK':
+        if g.azul == 'OK' and g.verde == 'OK' and g.vermelho == 'OK':
+            g.win_screen()
+        else:
             g.game_over()
 
 pygame.quit
